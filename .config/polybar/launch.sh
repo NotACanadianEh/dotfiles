e45a28c3aaeefad903f723ea066b8b8f235d3cc4
Nathan Hughes <nathan1hughes@gmail.com>
2018-04-22 16:53:43 +0100

    Smarter, better

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env sh
 
 # Terminate already running bar instances
-killall -q polybar
+killall polybar
 
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-polybar example
+polybar top &
+polybar bottom &
 
 echo "Bars launched..."
