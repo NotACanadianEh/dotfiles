4747ef99d80a765062edcd4e84de68c02b73f96d
Nathan Hughes <nathan1hughes@gmail.com>
2018-04-23 18:04:03 +0100

    Added a better dropbox script

---
+++ .config/polybar/dropbox.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import subprocess
+from sys import argv
+
+
+def shell_output(cmd):
+    result = subprocess.run(
+        cmd, stdout=subprocess.PIPE).stdout.decode('utf-8').rstrip()
+    return result
+
+
+if len(argv) > 1:
+    if argv[1] == 'start':
+        subprocess.Popen(['dropbox', 'start'])
+    elif argv[1] == 'stop':
+        subprocess.Popen(['dropbox', 'stop'])
+
+status = shell_output(['dropbox', 'status'])
+status = status.replace('Dropbox ', '')
+print(' Dropbox: {0}'.format(status))
